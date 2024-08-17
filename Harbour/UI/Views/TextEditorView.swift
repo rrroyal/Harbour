3a87a8397d7187a2dee676aad8fa0041172922b9
rrroyal <royal@shameful.xyz>
2024-08-17 18:10:12 +0200

    TextEditorView

---
+++ Harbour/UI/Views/TextEditorView.swift
@@ -0,0 +1,114 @@
+//
+//  TextEditorView.swift
+//  Harbour
+//
+//  Created by royal on 17/08/2024.
+//  Copyright © 2024 shameful. All rights reserved.
+//
+
+import CommonHaptics
+import SwiftUI
+
+// MARK: - TextEditorView
+
+struct TextEditorView: View {
+	@Environment(\.dismiss) private var dismiss
+	@Binding private var backingText: String
+	@State private var text: String
+	@State private var selection: TextSelection?
+	@ScaledMetric(relativeTo: .body) private var fontSize: Double = 12
+	@FocusState private var textFieldFocused: Bool
+	private let navigationTitle: String
+
+	init(text: Binding<String>, navigationTitle: String) {
+		self._text = .init(initialValue: text.wrappedValue)
+		self._backingText = text
+		self.navigationTitle = navigationTitle
+	}
+
+	var body: some View {
+		let textIsSame = text == backingText
+
+		NavigationStack {
+			TextEditor(text: $text, selection: $selection)
+				.font(.system(size: fontSize))
+				.fontDesign(.monospaced)
+				#if os(iOS)
+				.keyboardType(.alphabet)
+				.textInputAutocapitalization(.never)
+				#endif
+				.autocorrectionDisabled()
+//				.textSelectionAffinity(.downstream)
+				.focused($textFieldFocused)
+				.addingCloseButton()
+				.toolbar {
+					ToolbarItem(placement: .primaryAction) {
+						Button {
+							Haptics.generateIfEnabled(.selectionChanged)
+							backingText = text
+							dismiss()
+						} label: {
+							Text("Generic.Save")
+						}
+						.disabled(textIsSame)
+						.animation(.default, value: textIsSame)
+					}
+
+					#if os(iOS)
+					ToolbarItem(placement: .keyboard) {
+						HStack {
+							Button {
+								insertAtCursor("\t")
+							} label: {
+								Label(String("⇥"), systemImage: "arrow.right.to.line")
+							}
+
+							Spacer()
+
+							Button {
+								textFieldFocused = false
+							} label: {
+								Label("Generic.DismissKeyboard", systemImage: "keyboard.chevron.compact.down")
+							}
+						}
+						.font(.footnote)
+						.labelStyle(.iconOnly)
+						.padding(.horizontal, -10)
+					}
+					#endif
+				}
+				.navigationTitle(navigationTitle)
+				#if os(iOS)
+				.navigationBarTitleDisplayMode(.inline)
+				#endif
+		}
+		#if os(iOS)
+		.onAppear {
+			selection = .init(insertionPoint: text.endIndex)
+			textFieldFocused = true
+		}
+		#endif
+	}
+}
+
+// MARK: - TextEditorView+Actions
+
+private extension TextEditorView {
+	func insertAtCursor(_ char: Character) {
+		guard let selection, case .selection(let range) = selection.indices else {
+			return
+		}
+
+		self.text.insert(char, at: range.lowerBound)
+
+		let indexBefore = text.index(range.lowerBound, offsetBy: 1)
+		self.selection = .init(insertionPoint: indexBefore)
+	}
+}
+
+// MARK: - Previews
+
+#Preview {
+	@Previewable @State var text: String = "Hello, world!"
+	TextEditorView(text: $text, navigationTitle: "Text Editor")
+}
