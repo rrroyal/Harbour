90940d282071c4880f3e319d33fe8e9536834aae
rrroyal <royal@shameful.xyz>
2025-11-26 14:46:46 +0100

    Remove prefix from Xcode CI changelog

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -7,10 +7,6 @@ if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
 	TESTFLIGHT_DIR_PATH=../TestFlight
 	mkdir $TESTFLIGHT_DIR_PATH
 
-	echo "" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-	echo "---" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-	echo "" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-
 	git fetch --deepen 5
 	TZ=UTC git log -5 --date=iso-local --pretty=format:"%s (%ad) [%h]" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 
